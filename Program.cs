3d015550c085052368ab260058c1f31babebcde1
Mohamed Alsadi <mohamedalsadi@Mohameds-MacBook-Pro.local>
2021-08-20 20:06:38 -0500

    created phone book class

--- Program.cs
+++ Program.cs
@@ -5,8 +5,16 @@ namespace SpriteProject
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        {                  
+            PhoneBook per = new PhoneBook();
+            Console.WriteLine("What is your full name?");
+            per.FullName = Console.ReadLine();
+            Console.WriteLine("What email would you like to use");
+            per.Email = Console.ReadLine();
+            Console.WriteLine("What number do you want to use");
+            per.PhoneNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {per.FullName}, Email:{per.Email}, Phone:{per.PhoneNumber}");
+        }   
     }
 }
+                                                  
